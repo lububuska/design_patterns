7ff3507ac301fdc0800a6bae7481346023a53c81
lububuska <119815794+lububuska@users.noreply.github.com>
2024-09-09 19:48:01 +0300

    solve method 3 variant 11

--- lab1/algorithms.rb
+++ lab1/algorithms.rb
@@ -26,3 +26,39 @@ def minimal_odd_digit(number)
 end
 
 puts minimal_odd_digit(26574)
+
+# Метод 3. Найти сумму всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа.
+
+# Нахождение суммы и произведения цифр числа
+def summa_and_comp_of_digits(number)
+  summa_of_digits = 0
+  comp_of_digits = 1
+  while number > 0
+    summa_of_digits += number % 10
+    comp_of_digits *= number % 10
+    number /= 10
+  end
+  return summa_of_digits, comp_of_digits
+end
+
+# Алгоритм Евклида для нахождения НОД
+def nod(number_1, number_2)
+  while number_2 != 0
+    number_1, number_2 = number_2, number_1 % number_2
+  end
+  return number_1
+end
+
+# Окончательный поиск суммы делителей
+def summa_of_dividers(number)
+  summa = 0
+  summa_of_digits, comp_of_digits = summa_and_comp_of_digits(number)
+  for divider in 1..number
+    if number % divider == 0 && nod(divider, summa_of_digits) == 1 && nod(divider, comp_of_digits) != 1
+      summa += divider
+    end
+  end
+  return summa
+end
+
+puts summa_of_dividers(16)
