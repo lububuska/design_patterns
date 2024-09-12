849bc5b4424b61c1db93b8f62138d5c327ca0942
lububuska <119815794+lububuska@users.noreply.github.com>
2024-09-12 15:03:12 +0300

    resolve menu task

--- lab1/arrays.rb
+++ lab1/arrays.rb
@@ -14,9 +14,7 @@ def minimal_element(array)
   return minimal
 end
 
-puts minimal_element([15, 8, 10, 55, 32, 6])
-
-# Нахождение первого положительного элемента
+# Нахождение номера первого положительного элемента
 
 def first_positive_element(array)
   if array.size == 0 
@@ -30,4 +28,34 @@ def first_positive_element(array)
   return nil
 end
 
-puts first_positive_element([-3, -5, -8, -1, -9])
+def read_array_from_file(file)
+  array = []
+  begin
+    File.open(file, 'r') do |file|
+      file.each_line do |line|
+        line.split(", ").each do |number|
+          array.push(number.to_i)
+        end
+      end
+    end
+  rescue 
+    puts "Введите существующий файл!"
+    exit -1
+  end
+  return array
+end
+
+# Написать программу, которая принимает как аргумент два значения. Первое значение говорит, какой из методов задачи 1 выполнить, второй говорит о том, откуда читать список аргументом должен быть написан адрес файла. Далее необходимо прочитать массив и выполнить метод.
+
+if ARGV.size == 2
+  method = ARGV[0].to_i
+  file = ARGV[1]
+  array = read_array_from_file(file)
+  if method == 1
+    puts "Минимальный элемент: #{minimal_element(array)}"
+  elsif method == 2
+    puts "Номер первого положительного элемента: #{first_positive_element(array)}"
+  else puts "Ошибка, введите номер 1 или 2!"
+  end
+else puts "Введите все аргументы программы!"
+end
