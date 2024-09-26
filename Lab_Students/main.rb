877ee67eea6ab65ed54fc5074b69082f4a0727c2
lububuska <masenko.maria2004@gmail.com>
2024-09-27 00:56:09 +0300

    rename method into to_s, add validation to unnecessary fields

--- Lab_Students/main.rb
+++ Lab_Students/main.rb
@@ -1,8 +1,8 @@
 require './objects_and_classes.rb'
-student1 = Student.new({surname: "Masenko", name: "Maria", fathername: "Sergeevna", id: 1, tg: "lububuska", email: "masenko.maria2004@gmail.com", git: "lububuska"})
-student2 = Student.new({surname: "Anikin", name: "Mark", fathername: "Andreevich", id: 2, tel: "324897619", tg: "corray9", email: "mark.corray.off@gmail.com", git: "Corray333"})
+student1 = Student.new({surname: "Masenko", name: "Maria", fathername: "Sergeevna", id: 1, tg: "lububuska", email: "masenko.maria2004@gmail.com", git: "github.com/lububuska"})
+student2 = Student.new({surname: "Anikin", name: "Mark", fathername: "Andreevich", id: 2, tel: "324897619", tg: "corray9", email: "mark.corray.off@gmail.com", git: "github.com/Corray333"})
 student3 = Student.new({surname: "Ivanov", name: "Ivan", fathername: "Ivanovich", id: 3, tel: "89184567210", tg: "ivan", email: "ivanTheBest@mail.ru"})
 
-student1.show_information()
-student2.show_information()
-student3.show_information()
+student1.to_s()
+student2.to_s()
+student3.to_s()
--- Lab_Students/objects_and_classes.rb
+++ Lab_Students/objects_and_classes.rb
@@ -16,8 +16,21 @@ class Student
   end
 
   def self.telephone_validation?(tel_phone)
-    tel_phone.nil? || /\A(\+?7|8)\d{10}\z/.match?(tel_phone)
+    tel_phone.nil? || /^(\+?7|8)\d{10}$/.match?(tel_phone)
   end
+
+  def self.telegram_validation?(telegram)
+    telegram.nil? || /^@[a-zA-Z0-9_]{5, 32}$/.match?(telegram)
+  end
+
+  def self.email_validation?(email)
+    email.nil? || /^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/.match?(email)
+  end
+
+  def self.git_validation?(git)
+    git.nil? || /^(https:\/\/)?(www\.)?github.com\/[a-zA-Z0-9_-]+$/.match?(git)
+  end
+
 
   def tel=(tel_phone)
     if !self.class.telephone_validation?(tel_phone)
@@ -26,7 +39,29 @@ class Student
     @tel = tel_phone
   end
 
-  def show_information()
+  def tg=(telegram)
+    if !self.class.telegram_validation?(telegram)
+      raise "Your telegram has invalid format!"
+    end
+    @tg = telegram
+  end
+
+  def email=(email)
+    if !self.class.email_validation?(email)
+      raise "Your email has invalid format!"
+    end
+    @email = email
+  end
+
+  def git=(git)
+    if !self.class.git_validation?(git)
+      raise "Your git has invalid format!"
+    end
+    @git = git
+  end
+
+
+  def to_s()
     puts "-----------------"
     puts "#{@id}. #{@surname} #{@name} #{@fathername}" if @id 
     puts "- #{@tel}" if @tel
