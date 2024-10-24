a4ccfc68f4546844ae7044c3d4d06c47479c4991
lububuska <119815794+lububuska@users.noreply.github.com>
2024-10-24 22:36:00 +0300

    task 3.1.22 add finding amount of minimal elements in range

--- Lab3_polymorphism_blocks_collections_tests/processing.rb
+++ Lab3_polymorphism_blocks_collections_tests/processing.rb
@@ -3,3 +3,11 @@ def find_same_elements(array1, array2)
   same_elements_count = (array1 & array2).size
   return same_elements_count
 end
+
+# Задача 1.22	Дан целочисленный массив и интервал a..b. Необходимо найти количество минимальных элементов в этом интервале.
+def find_amount_of_minimal_elements(array, a, b)
+  minimal_element = array.min
+  puts minimal_element
+  amount = array[a..b].count(minimal_element)
+  return amount
+end
