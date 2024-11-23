d375d09599c3e9c6b36e5856b2999951249df075
lububuska <119815794+lububuska@users.noreply.github.com>
2024-11-23 10:49:45 +0300

    lab 4 task 4.1.2 add methods to get element by col and row, add row_count and col_count

--- Lab_Students/Data/data_table.rb
+++ Lab_Students/Data/data_table.rb
@@ -3,6 +3,25 @@ class Data_table
     @data = data
   end
 
+  def row_count
+    return data.size
+  end
+
+  def col_count
+    if data.empty?
+      return 0
+    end
+    return self.data[0].size
+  end
+
+  def get_element(row, col)
+    if row >= 0 && row < data.size-1 && col >= 0 && col < data[row].size-1
+      return self.data[row][col]
+    else
+      raise ArgumentError, "Index if out of range"
+    end
+  end
+
   private 
   attr_reader :data
 
