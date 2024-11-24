65a1e6376d597a94a4adb87ef11dd69afa8cef67
lububuska <119815794+lububuska@users.noreply.github.com>
2024-11-24 12:53:26 +0300

    delete setter from class Data_table

--- Lab_Students/Data/data_table.rb
+++ Lab_Students/Data/data_table.rb
@@ -24,12 +24,4 @@ class Data_table
 
   private 
   attr_reader :data
-
-  def data=(data)
-    if data.is_a?(Array) && data.all? {|row| row.is_a?(Array)}
-      @data = data
-    else
-      raise ArgumentError, "Data must be a 2-dimensional array"
-    end
-  end
 end
